78a1b2319ffe89a62a410ef6e221c83f9e5c9cf3
Sosuke1019 <kato.052wazime@gmail.com>
2023-04-14 20:47:15 +0900

    add main.py, app

--- app/__init__.py
+++ app/__init__.py
@@ -50,8 +50,3 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-
-if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
