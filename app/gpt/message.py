700c42436880c521e28497a77eb941129032309c
Sosuke1019 <kato.052wazime@gmail.com>
2023-04-19 16:35:44 +0900

    fix requirements.txt add README

--- app/gpt/message.py
+++ app/gpt/message.py
@@ -10,9 +10,11 @@ class Message:
     role: Role
     content: str
 
+    # -> Dict[str, str] はこのメソッドが辞書型を返すことを示す
     def to_dict(self) -> Dict[str, str]:
         return {"role": self.role.value, "content": self.content}
     
     @classmethod
+    # 辞書型を受け取ってMessageオブジェクトを精製するためのメソッド
     def from_dict(cls, message: Dict[str,str]) -> Message:
         return cls(role=Role(message["role"]), content=message["content"])
